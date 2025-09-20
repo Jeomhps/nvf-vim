85ea4ac8ffe9539f980608968a97ec6df0891741
Jeomhps <170039650+Jeomhps@users.noreply.github.com>
2025-09-20 22:40:07 +0000

    fix : issue with import for vim.nix

--- settings/vim.nix
+++ settings/vim.nix
@@ -1,19 +1,21 @@
-{...}: {
-  enableLuaLoader = true;
-  spellcheck = {
-    enable = true;
-    languages = ["en" "fr"];
-  };
-  options = rec {
-    termguicolors = true;
-    cursorlineopt = "both";
-    shiftwidth = 2;
-    tabstop = 2;
-    softtabstop = 2;
-  };
-  theme = {
-    enable = true;
-    name = "catppuccin";
-    style = "mocha";
+{
+  config.vim = {
+    enableLuaLoader = true;
+    spellcheck = {
+      enable = true;
+      languages = ["en" "fr"];
+    };
+    options = rec {
+      termguicolors = true;
+      cursorlineopt = "both";
+      shiftwidth = 2;
+      tabstop = 2;
+      softtabstop = 2;
+    };
+    theme = {
+      enable = true;
+      name = "catppuccin";
+      style = "mocha";
+    };
   };
 }
