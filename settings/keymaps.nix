3c8b6809ed0ecf3ca06d9e21a4c04ae75251e46c
Jeomhps <170039650+Jeomhps@users.noreply.github.com>
2025-10-06 11:27:17 +0200

    feat : add keymaps nix file

---
+++ settings/keymaps.nix
@@ -0,0 +1,11 @@
+{
+  config.vim.keymaps = [
+    {
+      key = "<leader>e";
+      mode = "n";
+      silent = true;
+      action = ":Neotree toggle<CR>";
+      description = "Toggle Neo-tree";
+    }
+  ];
+}
